2d5c78ea006a34b63b2068e93dfe599d8b72ab7c
Olayode Enoch <olayodeenoch@gmail.com>
2021-10-14 16:32:33 +0100

    Work created

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+  <div class="container">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+    <strong>Copyright &copy; <?php echo date('Y') ?> | Developed By <a href="https://wa.me/+2348074303809" target="_blank">Olayode Enoch</a></strong>
+  </div>
+  <!-- /.container -->
+</footer>
